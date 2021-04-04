f0ff8ba7b2edd1375f52f0a150bde256bc9f0b20
TonyJem <a.jemanov@gmail.com>
2021-04-04 18:33:46 +0300

    Add BalanceViewController and refactor project file structure

--- RealmHackatonTask/RealmHackatonTask/HomeScreen/HomeViewController.swift
+++ RealmHackatonTask/RealmHackatonTask/HomeScreen/HomeViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+//    TODO: currentBalance is not used yet
     var currentBalance = 888
     
     private var actionViewController: UIViewController {
@@ -36,3 +37,13 @@ private extension HomeViewController {
         self.show(actionViewController, sender: nil)
     }
 }
+
+/* TODO:
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destination.
+    // Pass the selected object to the new view controller.
+}
+*/
