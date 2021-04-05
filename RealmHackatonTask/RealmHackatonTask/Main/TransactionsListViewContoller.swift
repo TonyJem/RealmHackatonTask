4f9a0882d6823fadc280b56d8a6e9a2029c6b8bb
TonyJem <a.jemanov@gmail.com>
2021-04-05 22:09:39 +0300

    Create 'transactionsTableView' outlet in 'TransactionsListViewContoller'

--- RealmHackatonTask/RealmHackatonTask/Main/TransactionsListViewContoller.swift
+++ RealmHackatonTask/RealmHackatonTask/Main/TransactionsListViewContoller.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class TransactionsListViewContoller: UIViewController {
 
+    @IBOutlet private weak var transactionsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
