e8c4531f4ceb6d154671f240719a2dfb13a26f5f
TonyJem <a.jemanov@gmail.com>
2021-04-05 22:42:46 +0300

    Add 'OperationModel'

---
+++ RealmHackatonTask/RealmHackatonTask/Models/TransactionModel.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+struct Operation {
+    let date: String
+    let operation: String
+    let type: String
+    let amount: Int
+}
+
+class OperationModel {
+    
+    var transactions:[Operation] = []
+    
+    func addTransaction() {
+    }
+    
+    func sortTransactions() {
+    }
+}
